0a5b929c6d9b7c39d7a4f9a4dadce1c77e2824b9
Henry Kimura <htkimura@gmail.com>
2024-12-16 19:10:46 -0300

    chore: add base css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
 import { NotFound } from './pages/NotFound'
+import './css/base.css'
+import './css/typography.css'
+import './css/variables.css'
 
 function App() {
   return (
