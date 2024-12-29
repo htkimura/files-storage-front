bc01334b6920f17f24a57b2c2c516d29846d9e03
Henry Kimura <htkimura@gmail.com>
2024-12-29 10:29:33 -0300

    feat: add initial user context

---
+++ src/contexts/user-context.tsx
@@ -0,0 +1,45 @@
+import { useStoredState } from '@/hooks/use-stored-state'
+import React, {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+} from 'react'
+
+interface UserContextType {
+  token: string | null
+  setToken: Dispatch<SetStateAction<UserContextType['token']>>
+  refreshToken: string | null
+  setRefreshToken: Dispatch<SetStateAction<UserContextType['refreshToken']>>
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined)
+
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+  const [token, setToken] = useStoredState<string | null>('token', null)
+  const [refreshToken, setRefreshToken] = useStoredState<string | null>(
+    'refresh_token',
+    null,
+  )
+
+  return (
+    <UserContext.Provider
+      value={{
+        token,
+        setToken,
+        refreshToken,
+        setRefreshToken,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export const useUser = () => {
+  const context = useContext(UserContext)
+  if (context === undefined) {
+    throw new Error('useUser must be used within a UserProvider')
+  }
+  return context
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
+import { Toaster } from './components/ui/toaster.tsx'
+import { UserProvider } from './contexts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
+      <Toaster />
     </BrowserRouter>
   </StrictMode>,
 )
