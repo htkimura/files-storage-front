a60870928fda9bff5902b2cdcb3877ea84deb908
Henry Kimura <htkimura@gmail.com>
2024-12-29 10:35:17 -0300

    feat: add initial home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 import { NotFound } from './pages/NotFound'
+import { HOME_PAGE_ROUTE } from './routes'
+import { Login } from './pages/Login'
 import './css/base.css'
 import './css/typography.css'
 import './css/variables.css'
+import { Home } from './pages/Home'
 
 function App() {
   return (
     <Routes>
+      <Route path={HOME_PAGE_ROUTE} element={<Home />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
