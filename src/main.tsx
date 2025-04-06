41e56735f2e15ed372c33464cc9be2ad4b44cc91
Henry Kimura <htkimura@gmail.com>
2025-04-06 18:41:46 -0300

    chore: setup react query

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,22 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import { Toaster } from './components/ui/toaster.tsx'
 import { UserProvider } from './contexts'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './config/query.ts'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
-      <Toaster />
-    </BrowserRouter>
-  </StrictMode>,
-)
+const Root = () => {
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <UserProvider>
+            <App />
+          </UserProvider>
+        </QueryClientProvider>
+        <Toaster />
+      </BrowserRouter>
+    </StrictMode>
+  )
+}
+
+createRoot(document.getElementById('root')!).render(<Root />)
