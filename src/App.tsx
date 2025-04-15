bcbf4a11034371dabb39021e249ca89197a1063e
Henry Kimura <htkimura@gmail.com>
2025-04-15 20:38:36 -0300

    feat: add auth rote

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom'
 import { NotFound } from './pages/NotFound'
 import { HOME_PAGE_ROUTE, LOGIN_PAGE_ROUTE } from './routes'
 import { Login } from './pages/Login'
@@ -6,12 +6,25 @@ import './css/base.css'
 import './css/typography.css'
 import './css/variables.css'
 import { Home } from './pages/Home'
+import { useUser } from './contexts'
+
+const AuthRoute = () => {
+  const { token } = useUser()
+
+  if (!token) {
+    return <Navigate to={LOGIN_PAGE_ROUTE} replace />
+  }
+
+  return <Outlet />
+}
 
 function App() {
   return (
     <Routes>
-      <Route path={HOME_PAGE_ROUTE} element={<Home />} />
       <Route path={LOGIN_PAGE_ROUTE} element={<Login />} />
+      <Route element={<AuthRoute />}>
+        <Route path={HOME_PAGE_ROUTE} element={<Home />} />
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
