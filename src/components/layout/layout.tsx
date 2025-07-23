158da52180babd4ea6c6ea8e82a53cf071712408
Henry Kimura <htkimura@gmail.com>
2025-07-22 22:01:12 -0300

    feat: implement toast in app

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,10 +2,12 @@ import { type FC, type PropsWithChildren } from 'react'
 import { SidebarProvider } from '../ui/sidebar'
 import { Header } from './header'
 import { Sidebar } from './sidebar'
+import { Toaster } from 'react-hot-toast'
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
+      <Toaster position="top-center" />
       <SidebarProvider>
         <Sidebar>
           <div className="flex flex-col h-screen w-full">
